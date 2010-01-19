60191f5df4cc8ee460b18353499f19147e4997f0
David Rio Deiros (drio) <deiros@bcm.edu>
2010-01-19 17:27:09 -0600

    Specify lsf resources via config

--- bfast.lsf.submit.rb
+++ bfast.lsf.submit.rb
@@ -17,15 +17,20 @@ lsf    = LSFDealer.new(config.input_run_name,
 cmds   = BfastCmd.new(config, splits)
 
 # Per each split, create the basic bfast workflow with deps
-one_machine = "rusage[mem=28000]span[hosts=1]"
-reg_job     = "rusage[mem=4000]"
+#reg_job     = "rusage[mem=4000]"
+#one_machine = "rusage[mem=28000]span[hosts=1]"
+re_match = config.match_lsf_resources
+re_local = config.local_lsf_resources
+re_post  = config.post_lsf_resources
+re_tobam = config.tobam_lsf_resources
+re_final = config.final_lsf_resources
 final_deps = []
 splits.each do |s|
 	sn  = s.split(".")[-2]
-	dep = lsf.add_job("match" , cmds.match(s), sn, one_machine)
-	dep = lsf.add_job("local" , cmds.local   , sn, one_machine, [dep])
-	dep = lsf.add_job("postp" , cmds.post    , sn, reg_job    , [dep])
-	dep = lsf.add_job("tobam" , cmds.tobam   , sn, reg_job    , [dep])
+	dep = lsf.add_job("match" , cmds.match(s), sn, re_match)
+	dep = lsf.add_job("local" , cmds.local   , sn, re_local , [dep])
+	dep = lsf.add_job("postp" , cmds.post    , sn, re_post  , [dep])
+	dep = lsf.add_job("tobam" , cmds.tobam   , sn, re_tobam , [dep])
 	#dep = lsf.add_job("sort"  , cmds.sort    , sn, reg_job    , [dep])
 	#dep = lsf.add_job("index1", cmds.index1  , sn, reg_job    , [dep])
 	#dep = lsf.add_job("index2", cmds.index2  , sn, reg_job    , [dep])
@@ -35,7 +40,7 @@ splits.each do |s|
 end
 
 # when all the previous jobs are completed, we can merge all the bams
-lsf.add_job("final_merge", cmds.final_merge, "", reg_job, final_deps)
+lsf.add_job("final_merge", cmds.final_merge, "", re_final, final_deps)
 
 lsf.create_file
 
