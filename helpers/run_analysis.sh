f8c08596241768a150f863b28f639fa54bf43a9e
David Rio Deiros (drio) <deiros@bcm.edu>
2010-03-17 15:11:32 -0500

    create that dir for later dumping the logs

--- helpers/run_analysis.sh
+++ helpers/run_analysis.sh
@@ -5,6 +5,7 @@ run_process="$prod_pipe/bin/script.run.process.sh"
 config_file="./bf.config.yaml"
 rnds=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 9 | head -1 | cut -c1-5`
 queue="normal"
+lsf_logs_dir="./lsf_logs"
 
 stuff_to_clean="lsf_logs/ output/ cluster_JOBS.sh "
 stuff_to_clean="$stuff_to_clean reads metric* track* rg.txt marked*.txt"
@@ -30,8 +31,8 @@ to_lsf()
 
   echo "#$job_name"
   cat <<-EOF
-  bsub -o ./lsf_logs/${job_name}.out \
-  -e ./lsf_logs/${job_name}.err $w_dep \
+  bsub -o $lsf_logs_dir/${job_name}.out \
+  -e $lsf_logs_dir/${job_name}.err $w_dep \
   -J $job_name  \
   -q $queue  \
   "$run_process $job_name '$cmd'"
@@ -44,6 +45,7 @@ EOF
 
 rm -rf lsf_logs/ track* output/ cluster_JOBS.sh reads metric* track* rg.txt marked*.txt
 mkdir -p ./reads
+mkdir -p $lsf_logs_dir
 
 run_name=`grep run_name $config_file | awk -F: '{print $2}' | sed 's/ //g'`
 name_split="$run_name.genfastq.$rnds"
