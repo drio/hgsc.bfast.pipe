4555d5d1570214c44d957c8c6134761cad8180e4
David Rio Deiros (drio) <deiros@bcm.edu>
2010-03-19 15:32:53 -0500

    clarifying the finding of the lib directory

--- lib/load_libs.rb
+++ lib/load_libs.rb
@@ -1,8 +1,11 @@
 # vim: set filetype=ruby expandtab tabstop=2 shiftwidth=2 tw=80
 
-dir = File.join(File.dirname(File.dirname($0)), "lib")
-puts "Lib dir: #{dir}"
-Dir[File.join(dir, "*.rb")].each do |file| 
+bin_dir  = File.dirname($0)
+main_dir = File.dirname(bin_dir)
+
+lib_dir = File.join(main_dir, "lib")
+puts "Lib dir: #{lib_dir}"
+Dir[File.join(lib_dir, "*.rb")].each do |file| 
   puts "Loading #{file}"
   require File.basename(file)
 end
