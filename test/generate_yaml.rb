8a98dbdf2a8044505310b1cea0b8f0842c538a90
David Rio Deiros <driodeiros@gmail.com>
2010-04-06 15:59:00 -0500

    we want to use the java plugins from our working copy

--- test/generate_yaml.rb
+++ test/generate_yaml.rb
@@ -9,9 +9,19 @@ chip_design  = "/stornext/snfs1/next-gen/software/hgsc/capture_designs" +
                "/HD_exome/HD2_exome_target_region.bed.seq"
 
 def fix_for_test(bf_config)
+  c_path = Dir.pwd
   bf_config.gsub!(/\/h\/hsap.36.1.hg18\/hsap_36.1_hg18.fa/, "/t/test/test.fa")
   %w{28000 8000 4000}.each {|n| bf_config.gsub!(/#{n}/, "400") }
   %w{8g 4g}.each           {|n| bf_config.gsub!(/#{n}/, "1g") }
+
+  bf_config.gsub!(/regen_jar:.+$/,
+                  "regen_jar: #{c_path}" + 
+                  "/java/bam.header.creation/bam.header.creation.jar")
+  bf_config.gsub!(/s_jar:.+$/,
+                  "s_jar: #{c_path}/java/BAMStats/BAMStats.jar")
+  bf_config.gsub!(/bam_reads_val_jar:.+$/,
+                  "s_jar: #{c_path}" + 
+                  "/java/raw.bam.reads.validator/raw.bam.reads.validator.jar")
   bf_config
 end
 
@@ -44,8 +54,9 @@ bf_config.gsub!(/__READS_DIR__/ , "./reads")
 bf_config.gsub!(/__OUTPUT_DIR__/, "./output")
 bf_config.gsub!(/__CD__/        , test_type.eql?("cap") ? chip_design : "" )
 
-# Change the reference from human to test reference to speed up
-# BAM generation. Also, change the LSF usage mode
+# 1. Change the reference from human to test reference to speed up BAM generation. 
+# 2. Change the LSF usage mode
+# 3. Change the paths to the java plugins (use the wc ones)
 bf_config = fix_for_test(bf_config)
 
 # Write the config yaml file to the test/{mp, fr, cap} directory
